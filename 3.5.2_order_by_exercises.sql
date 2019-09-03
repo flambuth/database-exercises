faa721348087437ed81a62fc80182bb8e1197f0c
Fredrick Lambuth <flambuth@gmail.com>
2019-09-03 16:53:01 -0500

    figure out how to regex out words that contain e in there

--- 3.5.2_order_by_exercises.sql
+++ 3.5.2_order_by_exercises.sql
@@ -4,4 +4,17 @@
 -- start from here
 SELECT first_name, last_name
 FROM employees
+ORDER BY first_name ASC; 
+
+-- sortin firt and last name
+SELECT first_name, last_name
+FROM employees
+ORDER BY first_name ASC, last_name DESC;
+
+-- sortin firt and last name. Last name now has primacy of sorting. 
+SELECT first_name, last_name
+FROM employees
 ORDER BY last_name DESC, first_name ASC;
+WHERE last_name REGEXP ''
+
+-- how do you REGEXP to accept any string that contains one string char?
