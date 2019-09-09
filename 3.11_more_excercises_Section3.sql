ec144d5123168bddbc8a0dbde9e8267dd7c700cc
Fredrick Lambuth <flambuth@gmail.com>
2019-09-09 16:02:19 -0500

    this is starting to feel deceptively comfortable

--- 3.11_more_excercises_Section3.sql
+++ 3.11_more_excercises_Section3.sql
@@ -1,4 +1,6 @@
 -- FORGET ABOUT temp tables. Go do some easier stuff
+
+-- 1
 -- What is the average replacement cost of a film? Does this change depending on the rating of the film?
 -- Figure out replacement cost
 
@@ -8,9 +10,39 @@ FROM film AS f
 ;
 
 -- Group them by rating to see if that makes a signifiat difference in average replacement cost
-SELECT f.rating, AVG(f.replacement_cost), AVG(favg.replacement_cost)
+-- I put them in order, but Id like to select a column that shows either the total average or the each groups difference from total average 
+SELECT f.rating, AVG(f.replacement_cost)
 FROM film AS f
-	JOIN film AS favg
+	RIGHT JOIN film AS favg
 	ON f.film_id = favg.film_id
 GROUP BY f.rating
+ORDER By AVG(f.replacement_cost)
 ;
+
+-- 2
+-- How many different films of each genre are in the database?
+SELECT c.name AS genre_name, COUNT(fc.film_id) AS filmsInGenre
+FROM film_category AS fc
+	JOIN category AS c
+	USING (category_id)
+GROUP BY fc.category_id
+;
+
+-- 3
+-- What are the 5 most frequently rented films?
+-- Each f.film_id has several inventory_ids
+
+-- This groups each filmTitle by the amount of rental IDs, sorted desc and limited to 5 entries
+SELECT f.title, COUNT(r.rental_id) AS timesTitleWasRented
+FROM film AS f
+	JOIN inventory AS inv 
+	USING (film_id)
+	JOIN rental AS r
+	ON inv.inventory_id = r.inventory_id
+GROUP BY f.title
+ORDER BY timesTitleWasRented DESC
+LIMIT 5
+;
+
+-- 4
+--
