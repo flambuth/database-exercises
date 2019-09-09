5467cac2542517ec6bdacf59e271fb6a9ff405a9
Fredrick Lambuth <flambuth@gmail.com>
2019-09-09 08:56:44 -0500

    Maybe before Having or Group By, Ill do aggregates first

--- Scratch_groupBy.sql
+++ Scratch_groupBy.sql
@@ -1,6 +1,19 @@
 -- We've got the rental_rate groups. We're dislaying the average rental duration for all the group members.
 
-SELECT rental_rate, AVG(rental_duration)
+SELECT rental_rate, AVG(rental_duration), COUNT(rental_rate), 
 FROM film
 GROUP BY rental_rate;
--- HAVING COUNT(*) > ;
+-- HAVING rental_duration > 5.0 
+;
+
+-- just some aggrates. Before moving into GROUP BY
+-- THE AGGREGATE of all the values in the rental_duration field
+SELECT AVG(rental_duration)
+FROM film
+;
+
+-- SUM, REQUIRES GROUP BY
+-- Groups them by title and shows the total potential rental fees the title can acquire
+SELECT title, sum(rental_rate*rental_duration) AS max_rental_charge
+FROM film
+GROUP BY title;
